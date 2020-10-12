e0caa8e52c6bbe545866f08f6859925031d666cc
MikeC <kiragumike@gmail.com>
2020-10-12 12:47:29 +0300

    uploaded images

--- images/admin.py
+++ images/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Location,Categories,Image
 
 # Register your models here.
-class ImageAdmin(admin.ModelAdmin):
-    filter_horizontal = ('Categories',)
+# class ImageAdmin(admin.ModelAdmin):
+#     filter_horizontal = ('Categories',)
 
 admin.site.register(Location)
-admin.site.register(categories)
-admin.site.register(Image,ImageAdmin)    
+admin.site.register(Categories)
+admin.site.register(Image)    
